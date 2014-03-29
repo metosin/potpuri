99a6da2f9663ee340526df247ee2a5495e97797a
Tommi Reiman <tommi@iki.fi>
2014-03-29 13:47:23 +0200

    path-vals & assoc-in-path-vals

--- src/potpuri/core.clj
+++ src/potpuri/core.clj
@@ -7,3 +7,20 @@
 (defmacro fn->>
   "Creates a function that threads on input with some->>"
   [& body] `(fn [x#] (some->> x# ~@body)))
+
+(defn path-vals
+  "Returns vector of tuples containing path vector to the value and the value."
+  [m]
+  (letfn
+    [(pvals [l p m]
+       (reduce
+         (fn [l [k v]]
+           (if (map? v)
+             (pvals l (conj p k) v)
+             (cons [(conj p k) v] l)))
+         l m))]
+    (pvals [] [] m)))
+
+(defn assoc-in-path-vals
+  "Re-created a map from it's path-vals extracted with (path-vals)."
+  [c] (reduce (partial apply assoc-in) {} c))
--- test/potpuri/core_test.clj
+++ test/potpuri/core_test.clj
@@ -2,9 +2,19 @@
   (:require [midje.sweet :refer :all]
             [potpuri.core :refer :all]))
 
-(fact "fn-> & fn->>"
+(facts "fn-> & fn->>"
   (let [inc-x  (fn-> :x inc)
         sum-doubled-vals (fn->> vals (map (partial * 2)) (apply +))
         m {:x 1 :y 2 :z 3}]
     (inc-x m) => 2
     (sum-doubled-vals m) => 12))
+
+(fact "path-vals & assoc-in-path-vals"
+  (let [original {:a {:b {:c 1
+                          :d 2}
+                      :e 3}}
+        target [[[:a :e] 3]
+                [[:a :b :d] 2]
+                [[:a :b :c] 1]]]
+    (path-vals original) => target
+    (assoc-in-path-vals target) => original))
