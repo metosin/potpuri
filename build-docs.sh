48c0ace181e0c398e1cf3a6ee3c1141e8d94dd12
Juho Teperi <juho.teperi@iki.fi>
2015-01-16 20:54:28 +0200

    Scrapt building docs from travis

--- build-docs.sh
+++ build-docs.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "true" ]]; then
-    echo "PR, not building docs."
-    exit 0
-fi
-
-deploy_url="https://${GH_TOKEN}@github.com/metosin/potpuri.git"
 rev=$(git rev-parse HEAD)
 
-git config user.name "$GIT_NAME"
-git config user.email "$GIT_EMAIL"
+if [[ ! -d doc ]]; then
+    git clone --branch gh-pages git@github.com:metosin/potpuri.git doc
+fi
+(
+cd doc
+git pull
+)
 
-git clone --branch gh-pages $deploy_url doc
 lein doc
 cd doc
 git add --all
 git commit -m "Build docs from ${rev}."
-git push -q $deploy_url gh-pages
+git push origin gh-pages
