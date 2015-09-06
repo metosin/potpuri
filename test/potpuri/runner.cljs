57180dd8b10153a1eb76b832611802b65fa63c75
Juho Teperi <juho.teperi@iki.fi>
2015-09-06 19:04:23 +0300

    Fix CI tests

--- test/potpuri/runner.cljs
+++ test/potpuri/runner.cljs
@@ -5,7 +5,13 @@
 
 (nodejs/enable-util-print!)
 
+(def status (atom nil))
+
 (defn -main []
-  (test/run-all-tests #"^potpuri.*-test$"))
+  (test/run-all-tests #"^potpuri.*-test$")
+  (js/process.exit @status))
+
+(defmethod test/report [:cljs.test/default :end-run-tests] [m]
+  (reset! status (if (test/successful? m) 0 1)))
 
 (set! *main-cli-fn* -main)
