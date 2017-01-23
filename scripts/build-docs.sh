d15c9982de1426d7049f50bfc93a10d7ac515359
Juho Teperi <juho.teperi@iki.fi>
2017-01-23 19:34:27 +0200

    Build docs for tags & master

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,7 +4,13 @@ set -euo pipefail
 
 rev=$(git rev-parse HEAD)
 remoteurl=$(git ls-remote --get-url origin)
-repodir=doc
+repodir=gh-pages
+tag=$(git tag --points-at HEAD)
+name=$tag
+if [[ -z $name ]]; then
+    name=master
+fi
+target=$repodir/$name
 
 git fetch
 if [[ -z $(git branch -r --list origin/gh-pages) ]]; then
@@ -26,7 +32,13 @@ else
     )
 fi
 
+rm -fr doc
 lein codox
+
+# replace docs for current version with new docs
+rm -fr "$target"
+cp -r doc "$target"
+
 cd "$repodir"
 git add --all
 git commit -m "Build docs from ${rev}."
