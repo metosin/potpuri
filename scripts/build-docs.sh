dc34ac90da70f5c90978d594b93f2079cc211533
Miikka Koskinen <miikka.koskinen@iki.fi>
2017-01-23 19:08:15 +0200

    Use the version of build-docs.sh that actually works

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+set -euo pipefail
+
 rev=$(git rev-parse HEAD)
 remoteurl=$(git ls-remote --get-url origin)
+repodir=doc
 
-if [[ ! -d doc ]]; then
-    git clone --branch gh-pages ${remoteurl} doc
+git fetch
+if [[ -z $(git branch -r --list origin/gh-pages) ]]; then
+    (
+    mkdir "$repodir"
+    cd "$repodir"
+    git init
+    git remote add origin "${remoteurl}"
+    git checkout -b gh-pages
+    git commit --allow-empty -m "Init"
+    git push -u origin gh-pages
+    )
+elif [[ ! -d gh-pages ]]; then
+    git clone --branch gh-pages "${remoteurl}" "$repodir"
+else
+    (
+    cd "$repodir"
+    git pull
+    )
 fi
-(
-cd doc
-git pull
-)
 
 lein codox
-cd doc
+cd "$repodir"
 git add --all
 git commit -m "Build docs from ${rev}."
 git push origin gh-pages
