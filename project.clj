7d8fe2cc696fe8026f51a2bdd54c95a60aa61fc9
Miikka Koskinen <miikka@metosin.fi>
2017-05-09 18:36:45 +0300

    Set version to 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/potpuri "0.4.0"
+(defproject metosin/potpuri "0.4.1-SNAPSHOT"
   :description "Common stuff missing from the clojure.core."
   :url "https://github.com/metosin/potpuri"
   :license {:name "Eclipse Public License"
